691247fd571ab233f4a54245ebb29d1792670083
Zac E <zacegleston@gmail.com>
2022-01-09 11:31:00 -0600

    FEF Challenge

---
+++ cookies.js
@@ -0,0 +1,20 @@
+// Create a site that allows the user to to create, edit, remove and
+// display cookies. Host your solution in a public Github repo. Present
+// your work to the server on 01/23/22 at 8PM UTC
+
+// Resources
+// https://www.w3schools.com/js/js_cookies.asp
+// https://morioh.com/p/86545e1a5365
+
+
+// BREAKDOWN
+
+// Allow a user to create cookies 
+// Allow user to edit cookies
+// Allow user to remove cookies
+// Allow user to display cookies
+
+// Create Cookie from user input 
+// Get user input and store in a cookie
+// document.cookie
+
