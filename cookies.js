bbec167e8ca1e62327be0a7d0df0e176263e465b
Zac E <zacegleston@gmail.com>
2022-01-23 10:10:04 -0600

    Stuck on Delete

--- cookies.js
+++ cookies.js
@@ -1,18 +1,11 @@
 document.querySelector('#createBtn').addEventListener('click', function() {
 	let newCookieName = prompt('Input name for your cookie');
-	// let newCookieValue = prompt('Input value for your cookie');
-	//let addedCookie = document.cookie;
 
-	// let addedCookie = ` ${newCookieName} = ${newCookieValue}`;
 	console.log(newCookieName);
-	// console.log(newCookieValue);
-	// console.log(addedCookie);
 
 	document.cookie = `${newCookieName}`;
 
-                        <td><input type="button" value="Delete Row" onclick=deleteRow()></td>
-
-
+	//                  <td><input type="button" value="Delete Row" onclick=deleteRow()></td>
 
 	function addRow(cookieTable) {
 		let tableRef = document.getElementById(cookieTable);
@@ -24,27 +17,18 @@ document.querySelector('#createBtn').addEventListener('click', function() {
 		let newText = document.createTextNode(`${newCookieName}`);
 
 		newCell.appendChild(newText);
-	}
 
-	function deleteRow() {
-		// event.target will be the input element.
-		var td = document.newRow.parentNode;
-		var tr = td.parentNode; // the row to be removed
-		tr.parentNode.removeChild(tr);
+		newCell.setAttribute('id', `${newCookieName}`);
 	}
 
 	addRow('cookieTable');
 });
 
-// let table = document.createElement('table');
-// let thead = document.createElement('thead');
-// let tbody = document.createElement('tbody');
-
-// table.appendChild(thead);
-// table.appendChild(tbody);
-
-// document.getElementById('body').appendChild(table);
-
 document.querySelector('#editBtn').addEventListener('click', function() {
 	let updateCookieName = prompt('Which cookie would you like to change?');
+});
+
+document.querySelector('#deleteBtn').addEventListener('click', function() {
+	const deleteCookie = prompt('Input name of cookie you want to delete');
+	document.cookie = `${deleteCookie}=; expires=Thu, 01 Jan 2015 00:00:00 UTC; path=/;`;
 });
